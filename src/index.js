bf6359a125b99a11ca8d25bf3d6ea3528b2c4305
Mlodzieniak <116485218+Mlodzieniak@users.noreply.github.com>
2023-01-12 21:22:23 +0100

    started settin html and css file

--- src/index.js
+++ src/index.js
@@ -8,7 +8,3 @@ const input = "Czalizard";
 const result = inputsAreValid(input);
 const myDiv = document.querySelector(".container");
 const niceText = document.createElement("h1");
-niceText.innerText = `Is ${input} a valid input? ${result}! XsD`;
-niceText.classList.add("purple");
-myDiv.appendChild(niceText);
-menuPage();
